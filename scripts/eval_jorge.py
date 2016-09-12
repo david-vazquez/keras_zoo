ed88cf50765c47d4f5cb8caaef601b81ebc36854
David Vazquez <dvazquez@cvc.uab.es>
2016-09-12 17:09:34 -0400

    Added jorge script for eval

--- scripts/eval_jorge.py
+++ scripts/eval_jorge.py
@@ -33,6 +33,7 @@ def my_label2rgboverlay(labels, colors, image, bglabel=None,
     return output
 
 
+# Save image
 def save_img(img, mask, result, fname, color_map, void_label):
     img = img / 255.
 
@@ -41,12 +42,19 @@ def save_img(img, mask, result, fname, color_map, void_label):
                                      image=img,
                                      bglabel=void_label,
                                      alpha=0.2)
-    combined_image = np.concatenate((img, label_mask, result),
-                                    axis=1)
+
+    label_result = my_label2rgboverlay(result,
+                                       colors=color_map,
+                                       image=img,
+                                       bglabel=void_label,
+                                       alpha=0.2)
+
+    combined_image = np.concatenate((img, label_mask, label_result), axis=1)
     scipy.misc.toimage(combined_image).save(fname)
 
+
 # Main function
-def main(dataset_path, results_path):
+def main(dataset_path, results_path, n_classes=4):
     # Get paths
     images_path = dataset_path + 'images/'
     masks_path = dataset_path + 'masks/'
@@ -55,6 +63,9 @@ def main(dataset_path, results_path):
 
     # Get testing files
     file_names = os.listdir(images_path)
+
+    # Create the confusion matrix
+    cm = np.zeros((n_classes, n_classes))
 
     # Process each file
     for name in file_names:
@@ -72,20 +83,35 @@ def main(dataset_path, results_path):
         else:
             result = io.imread(results_cvc612 + str(file_int) + '.tif')
 
+        # Make y_true and y_pred flatten
+        y_true = mask.flatten()
+        y_pred = result.flatten()
 
-        print ('Image shape: ' + str(image.shape))
-        print ('Mask shape: ' + str(mask.shape))
-        print ('Result shape: ' + str(result.shape))
+        # Fill the confusion matrix
+        for i in range(n_classes):
+            for j in range(n_classes):
+                cm[i, j] += ((y_pred == i) * (y_true == j)).sum()
+
+        # print ('Image shape: ' + str(image.shape))
+        # print ('Mask shape: ' + str(mask.shape))
+        # print ('Result shape: ' + str(result.shape))
 
         # Show result
         color_map = sns.hls_palette(5)
         fname = './res.png'
-        save_img(image, mask, result, fname, color_map, 4)
-        exit()
+        save_img(image, mask, result, name, color_map, 4)
 
+    # Compute metrics
+    TP_perclass = cm.diagonal().astype('float32')
+    jaccard_perclass = TP_perclass/(cm.sum(1) + cm.sum(0) - TP_perclass)
+    jaccard = np.nanmean(jaccard_perclass)
+    accuracy = TP_perclass.sum()/cm.sum()
+    print ('Accuracy: ' + str(accuracy))
+    print ('Jaccard: ' + str(jaccard))
+    print ('Jaccard per class: ' + str(jaccard_perclass))
 
 
 # Entry point of the script
 if __name__ == '__main__':
-    main(dataset_path='/data/lisa/exp/vazquezd/datasets/polyps_split2/CVC-912/test/',
-         results_path='/data/lisa/exp/vazquezd/datasets/polypsBaseline/')
+    main(dataset_path='/data/lisa/exp/vazquezd/datasets/polyps_split2/CVC-912/valid/',
+         results_path='/data/lisa/exp/vazquezd/datasets/polypsBaseline2/')
